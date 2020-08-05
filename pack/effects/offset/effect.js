e0a88eed0b1ab1b040866dcbba53fa1b793924c7
qeaml <qeaml@wp.pl>
2020-08-05 10:19:25 +0200

    � Replace flip effect with offset

--- pack/effects/offset/effect.js
+++ pack/effects/offset/effect.js
@@ -1,10 +1,10 @@
 function effect(x, y, pixel) {
     x += offset;
     y += offset;
-    if(x >= size.x) {
+    if(x >= size.x + jump.x) {
         x -= size.x;
     }
-    if(y >= size.y) {
+    if(y >= size.y + jump.y) {
         y -= size.y;
     }
     return ImageAt(x, y);
